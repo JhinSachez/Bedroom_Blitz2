34b28b1f517acbf4e93f95880677e4f8ea7c922a
JhinSachez <manoloserrano2010@hotmail.com>
2024-06-09 20:12:38 -0600

    falta regreso a menu principal, musica y un techo

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] private int min, seg;
     [SerializeField] private TextMeshPro timer;
+    public FadeScreen fadeScreen;
+
     //[SerializeField] private GameObject timeOverPanel;
     //[SerializeField] private GameObject victoryPanel;
     //[SerializeField] private GameObject checkMark;
-    private float timeLeft;
+    public float timeLeft;
     private bool ongoing;
-    
     private void Awake()
     {
         timeLeft = (min * 60) + seg;
@@ -28,24 +31,28 @@ public class Timer : MonoBehaviour
             {
                 ongoing = false;
                 timeLeft = 0;
-                Time.timeScale = 0f;
+                //Time.timeScale = 0;
+                SceneManager.LoadScene(2);
                 //timeOverPanel.SetActive(true);
             }
             int tempMin = Mathf.FloorToInt(timeLeft / 60);
             int tempSeg = Mathf.FloorToInt(timeLeft % 60);
             timer.text = string.Format("{00:00}:{01:00}", tempMin, tempSeg);
     
-            /*if(ClearCon._score == 5) //cambiar numero dependiendo de objetos en el mundo
+            if(Sorting._sodaScore == 5) //cambiar numero dependiendo de objetos en el mundo
             {
                 ClearCondition();
-                victoryPanel.SetActive(true);
-            }*/
+            }
         }
     }
     
-    /*private void ClearCondition()
+
+    private void ClearCondition()
     {
         ongoing = false;
         Time.timeScale = 0f;
-    }*/
+        SceneManager.LoadScene(3);
+    }
+
+    
 }
